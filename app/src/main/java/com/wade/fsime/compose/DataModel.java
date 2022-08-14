4ff5a2937357b926becabf572ddfaf417d668962
wade <wade.fs@gmail.com>
2022-08-14 21:57:44 +0800

    參考 https://www.digitalocean.com/community/tutorials/android-listview-with-custom-adapter-example-tutorial, 欠把 CustomAdapter 加入 Preference 中

---
+++ app/src/main/java/com/wade/fsime/compose/DataModel.java
@@ -0,0 +1,13 @@
+package com.wade.fsime.compose;
+
+public class DataModel {
+    String compose_item;
+
+    public DataModel(String compose_item) {
+        this.compose_item = compose_item;
+    }
+
+    public String getComposeItem() {
+        return compose_item;
+    }
+}
