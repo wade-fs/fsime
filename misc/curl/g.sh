df172a22680cf8dea76e8ad8412ed04068801744
wade <wade.fs@gmail.com>
2022-12-29 19:25:02 +0800

    remove misc/ database/

--- misc/curl/g.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-COUNT=0
-while read ch; do
-	COUNT=$((COUNT + 1))
-	curl "https://pedia.cloud.edu.tw/Home/List/1?search=${ch}&source=withPrimary,withEOL,withMH,withConcise,withIdiom,withJunior,withRevised&order=relevance&size=100" -o ${COUNT}-${ch}.txt
-	cat ${COUNT}-${ch}.txt | grep -rHn 'a class="searchResultWord" index=' | sed 's/.*title=//g;s/&search.*//g' | grep ${ch} | grep -v '^.$' > ../words/${COUNT}-${ch}.txt
-done
