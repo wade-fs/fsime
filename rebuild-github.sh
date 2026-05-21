c5d9fb4c59fc7a4dbd75237a6e628678856d6135
wade <wade.fs@gmail.com>
2026-05-22 07:16:14 +0800

    Add comprehensive secret checks for Release

--- rebuild-github.sh
+++ rebuild-github.sh
@@ -2,6 +2,6 @@ TAG=$1
 [ -z "$TAG" ] && echo "Usage: $0 TAG" && exit 1
 
 git tag -d $TAG
-git push github :refs/tags/$TAG
+git push origin :refs/tags/$TAG
 git tag $TAG
-git push github $TAG
+git push origin $TAG
