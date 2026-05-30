032bf1bf7c6a034131c49716b1f8823c3fc0965d
wade <wade.fs@gmail.com>
2026-05-30 17:29:37 +0800

    update README.md

--- github-rebuild.sh
+++ github-rebuild.sh
@@ -3,6 +3,6 @@ TAG=$1
 [ -z "$TAG" ] && echo "Usage: $0 TAG" && exit 1
 ! [[ "$TAG" =~ ^v[0-9]\.[0-9]$ ]] && echo "Usage: $0 v#.#" && exit 1
 git tag -d $TAG >/dev/null 2>&1 || true
-git push origin :refs/tags/v1.1
+git push origin :refs/tags/$TAG
 git tag $TAG
 git push origin $TAG --force
